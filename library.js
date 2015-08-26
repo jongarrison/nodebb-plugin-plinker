d604dd0bd74e7487b406af00edaddd737d1ce506
Jon Garrison <jongarrison@gmail.com>
2015-08-26 12:38:13 -0700

    Custom topic route working

--- library.js
+++ library.js
@@ -4,27 +4,37 @@
 	var winston = module.parent.require('winston'),
 		async = module.parent.require('async'),
 		nconf = module.parent.require('nconf'),
-
 		Meta = module.parent.require('./meta'),
 		User = module.parent.require('./user'),
 		Posts = module.parent.require('./posts'),
 		Topics = module.parent.require('./topics'),
 		Privileges = module.parent.require('./privileges'),
-		SocketPosts = module.parent.require('./socket.io/posts'),
-		Plinker = {
+		SocketPosts = module.parent.require('./socket.io/posts');
+
+			//Stuff for topic rendering
+			//app = express(),
+			//controllers = module.parent.require('./controllers'),//possible double dot ../controllers
+			//middleware = module.parent.require('./middleware'),
+
+
+		var	Plinker = {
 			config: function() {
 				winston.info("PLINKER - config called");
 			},
 
-			init: function (data, callback) {
-				// Load saved config
+			staticAppLoad: function(params, callback) {
+				var app = params.app,
+						router = params.router,
+						middleware = params.middleware,
+						controllers = params.controllers;
 
-				winston.info("PLINKER - init called");
+
+				// Load saved config
+				winston.info("PLINKER - app.load called");
 				winston.info("PLINKER - nconf:" + nconf.get('url'));
 
 				winston.info("About to register route for rendered page");
-
-				var render = function(req, res) {
+				var renderTestTemplate = function(req, res) {
 					var templatePath = 'nodebb-plugin-plinker/test';
 					winston.info("About to render template: " + templatePath)
 
@@ -33,10 +43,23 @@
 						jgdata: "Some data rendered into things"
 					});
 				};
+				router.get('/plinker-test-page', renderTestTemplate);
 
-				data.router.get('/plinker-test-page', render);
+				var helpers = module.parent.require('./routes/helpers');
+				var setupPageRoute = helpers.setupPageRoute;
+				//winston.info("controllers is: " + JSON.stringify(controllers));
+				var topicController = controllers.topics;
+				//winston.info("topicController is: " + JSON.stringify(topicController));
+				setupPageRoute(router, '/plugin-topic/:topic_id/:slug?', middleware, [], topicController.get);
 
+				router.get('/plinker-reset-template-cache', function(req, res) {
+					winston.info("Attempting to reset template cache");
+					var templates = module.parent.require('templates');
+					templates.compile();
+					res.sendStatus(200);
+				});
 
+				//Done
 				if (typeof callback === 'function') {
 					callback();
 				}
